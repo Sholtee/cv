de677878e660cb35aac3ef67ed2bdc71bfad289e
Dénes Solti <sodnaatx@gmail.com>
2020-09-02 15:00:02 +0200

    [done] remove unused dependencies

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,14 +6,10 @@ import Vue from 'vue';
 import App from '../components/App.vue';
 import titleMixin from './mixins/title';
 import capitalize from './filters/capitalize';
-import _ from 'underscore';
 
 Vue.config.productionTip = false;
-
 Vue.mixin(titleMixin);
-
 Vue.filter('capitalize', capitalize);
-Vue.filter('orderBy', _.sortBy);
 
 new Vue({
   render: h => h(App)
