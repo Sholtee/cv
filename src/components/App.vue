e612a96cef2a9d5093bcfac1f555a687a2b4f22d
Dénes Solti <sodnaatx@gmail.com>
2020-09-02 10:49:42 +0200

    [done] test data rendering

--- src/components/App.vue
+++ src/components/App.vue
@@ -5,15 +5,19 @@
 
 <template lang="pug">
 #app
+    table
+        CV-Heading(v-for="(data, title) in content.headings" :key="title" :title="title" :content="data")
 </template>
 
 <script>
 import content from  '../assets/content.json';
+import heading from './CV-Heading.vue'
 
 export default {
   name: 'CV',
   title: 'CV',
   components: {
+    "CV-Heading": heading
   },
   data: function() {
     return {
@@ -37,4 +41,20 @@ export default {
     left: 0
     top: 0
     font-family: Roboto, sans-serif
+
+    > table
+        position: relative
+        border-collapse: collapse
+        background: white
+        overflow: hidden
+        width: $table-width
+        max-width: 100%
+        margin: 0 auto $min-margin
+        text-align: center
+        font-size: 1rem
+        border: none
+
+        @media (max-width: #{$table-width})
+            margin-left: $min-margin
+            margin-right: $min-margin
 </style>
---
+++ src/components/CV-Heading.vue
@@ -0,0 +1,71 @@
+<!--
+   CV-Heading.vue
+   Author: Denes Solti
+-->
+
+<template lang="pug">
+//-
+  Vue template-nek csak egy gyoker eleme lehet ezert a <table> alatt tobb <tbody> elemunk lesz ami teljesen valid:
+  https://developer.mozilla.org/en-US/docs/Web/HTML/Element/tbody
+tbody
+  tr.group-header
+    th {{title | capitalize}}
+    th.empty
+  tr(v-for="(value, key) in content")
+    td.key {{key | capitalize}}
+    td.value(v-html="value")
+</template>
+
+<script>
+  export default {
+  name: 'CV-Heading',
+  props: {
+    title: String,
+    content: Object
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import "../styles/consts"
+
+*
+    position: relative
+
+tbody > tr
+    color: $font-color
+    line-height: 1.2
+    height: 3.125rem
+    font-weight: unset
+
+    &:not(.group-header)
+        will-change: background-color
+        transition: background-color $default-transition-len
+
+        &:hover
+            color: $font-color-highlighted
+            background-color: $row-bg
+
+        > .key
+            text-align: right
+
+        > .value
+            text-align: left
+
+    &.group-header
+        font-size: 1rem
+        border-bottom: 1px solid black
+
+        > th
+            font-weight: bold
+            padding: $min-margin
+            text-align: left
+
+            &:not(.empty)
+                width: 30%
+                background: #e6e6e6
+
+    > th, td
+        &:first-of-type
+            border-right: 1.7rem solid transparent
+</style>
