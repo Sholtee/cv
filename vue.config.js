349644df9d2afb1058fbd5f791bf8951007dcea5
denes.solti <sodnaatx@gmail.com>
2024-12-29 14:39:30 +0100

    don't use source map in prod builds

--- vue.config.js
+++ vue.config.js
@@ -28,7 +28,7 @@ module.exports = {
         }
       ]
     },
-    devtool: 'source-map'
+    devtool: process.env.NODE_ENV === 'development' ? 'source-map' : false
   },
   pages: {
     index: {
